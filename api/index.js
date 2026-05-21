8e27f8ff752e07792f23b64616b117f283ecebec
nahid28888 <mdnahidhassan29999@gmail.com>
2026-05-21 12:48:35 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,14 +11,15 @@ module.exports = async (req, res) => {
         return res.status(403).send("Invalid Token or Account Blocked!");
     }
 
-    // ২. তোমার প্লেলিস্ট ডেটা (সরাসরি কোডের ভেতর বসিয়ে দিলাম)
-    // এখানে তোমার m3u ফাইলের সব লিংকগুলো নিচের মতো করে বসিয়ে দাও
+    // ২. তোমার প্লেলিস্ট ডেটা (ব্যাকটিক দিয়ে শুরু ও শেষ করা হয়েছে)
     const playlistData = `#EXTM3U
 #EXTINF:-1 tvg-logo="https://tvassets.roarzone.net/images/ananda-tv.png" group-title="bangla",ANANDA TV
 https://edge2.roarzone.net:8447/roarzone/edge5/ananda-tv/index.m3u8?token=2350fde503b77fe19ec883982f262e557eb2d32c-7eabcc5d78fbb419fc6887eeffd338b2-1779338613-1779327813
 #EXTINF:-1 tvg-logo="https://tvassets.roarzone.net/images/asian-tv.png" group-title="bangla",ASIAN TV
 https://edge2.roarzone.net:8447/roarzone/edge5/asian-tv/index.m3u8?token=c43bae1126a23cab28b8cd3ac97a2b47f34dbdd5-d37ccae0ded19c878cb1f3bb7abb9a50-1779338615-1779327815
 #EXTINF:-1 tvg-logo="https://tvassets.roarzone.net/images/atn-bangla.png" group-title="bangla",ATN BANGLA
+https://edge2.roarzone.net:8447/roarzone/edge5/atn-bangla/index.m3u8?token=তোমার_বাকি_টোকেন_এখানে`; 
+
     // ৩. রেসপন্স পাঠানো
     res.setHeader('Content-Type', 'audio/x-mpegurl');
     return res.status(200).send(playlistData);
