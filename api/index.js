309366f6ef223a8c187d9df94e18e907fe79e46b
nahid28888 <mdnahidhassan29999@gmail.com>
2026-05-21 19:16:34 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
 
     if (!token) return res.status(403).send("Error: Token missing!");
 
-    const allowedTokens = ["nahid3link", "rahat3link", "yousuf3link"];
+    const allowedTokens = ["nahid3link", "rahat3link", "yousuf3link", "anik3link", "milon3link"];
 
     if (!allowedTokens.includes(token)) {
         return res.status(403).send("Invalid Token or Account Blocked!");
